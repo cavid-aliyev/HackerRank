4348d82f8d8b04f8a4d06088f1b8affd82d2d1eb
Cavid Aliyev <69758071+cavid-aliyev@users.noreply.github.com>
2020-09-18 17:50:52 +0400

    Integers Come In ALL Sizes

---
+++ integercome.py
@@ -0,0 +1,11 @@
+# Integers Come In ALL Sizes -> https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+sum1 = pow(a,b)
+sum2 = pow(c,d)
+
+print(sum1 + sum2)
