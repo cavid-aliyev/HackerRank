1dc6281f0091b9e19ea84436a9fc10bd30832269
Cavid Aliyev <69758071+cavid-aliyev@users.noreply.github.com>
2020-10-25 09:19:17 +0400

    Dot and Cross

---
+++ dotandcross.py
@@ -0,0 +1,12 @@
+# Dot and Cross -> https://www.hackerrank.com/challenges/np-dot-and-cross/problem
+
+import numpy
+n = int(input())
+
+a = numpy.array([input().split() for _ in range(n)], int)
+b = numpy.array([input().split() for _ in range(n)], int)
+print(numpy.dot(a, b))
+
+
+
+
